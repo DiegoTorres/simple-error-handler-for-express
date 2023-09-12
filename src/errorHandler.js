fb37cdda643b31f6bb19caf886b425268a140cef
Diego Torres Iglesias <diegotorres.rj@gmail.com>
2023-09-12 13:37:06 -0300

    Fix, adds default error message to internalServerError.

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -9,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
   res.removeHeader('X-Powered-By');
   res.removeHeader('Server');
 
-  if (!(err instanceof BaseError)) error = new InternalServerError();
+  if (!(err instanceof BaseError)) error = new InternalServerError(err.message);
 
   return res.status(error.statusCode).json(error.serializedError());
 };
